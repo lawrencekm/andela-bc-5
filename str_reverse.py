32b46873a7d95cfd08043d2d2a3be649be0382e6
lawrence <lawrencekm04@gmail.com>
2016-01-26 15:05:04 +0300

    add new reverse unction

--- str_reverse.py
+++ str_reverse.py
@@ -5,3 +5,13 @@ word = "andela"
 print reverse(word)
 print reverse("lawrence")
 print reverse("android")
+
+
+def reversex(s):
+	s = list(s)
+	for i in range(len(s) // 2): # temp = s[i]
+		s[i], s[len(s) - 1 - i] = s[len(s) - 1 - i], s[i]
+# s[len(s) - 1 - i] = temp
+	return " ".join(s)
+
+print reverse("kamau")
